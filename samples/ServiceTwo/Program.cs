3769c1e4ec0f0f24ea3d0272c1b37d2f2868d02c
Yasin Mahammad <yasin.uohyd@gmail.com>
2019-01-05 12:55:47 +0530

    Added ServiceTwo

---
+++ samples/ServiceTwo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
